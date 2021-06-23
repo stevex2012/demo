dbc0c981c4917ea07b18a384aef070e0d123f94b
stevex2012 <18725873459@163.com>
2021-06-23 09:48:59 +0800

    learn esbuild

---
+++ esbuild/app.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as Server from 'react-dom/server'
+
+
+let Greet = () => <h1>hello world</h1>
+
+console.log(Server.renderToString(<Greet />))
---
+++ esbuild/build.js
@@ -0,0 +1,6 @@
+require('esbuild').build({
+  entryPoints: ['app.jsx'],
+  bundle: true,
+  outfile: 'out.js',
+  sourcemap: true,
+}).catch(() => process.exit(1))
