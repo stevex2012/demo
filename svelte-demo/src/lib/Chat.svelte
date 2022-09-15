3ec0dd7752d43cb9eca6d42b6509117ad9c0780d
stevex2012 <18725873459@163.com>
2022-09-15 10:19:56 +0800

    svelte

---
+++ svelte-demo/src/lib/Chat.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Eliza from "elizabot";
+  import { beforeUpdate, afterUpdate } from "svelte";
+
+  const eliza = new Eliza();
+  let comments = [
+    {
+      author: "eliza",
+      text: eliza.getInitial(),
+    },
+  ];
+</script>
+
+<div>
+  {#each comments as { author, text }}
+    <div>{text}</div>
+  {/each}
+  <input type="text" />
+</div>
--- svelte-demo/src/pages/about/About.svelte
+++ svelte-demo/src/pages/about/About.svelte
@@ -1,18 +1,7 @@
-<main>about345435</main>
+<script>
+  import Chat from "../../lib/Chat.svelte";
+</script>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div>
+  <Chat />
+</div>
